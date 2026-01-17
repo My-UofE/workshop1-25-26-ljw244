84f5157b94c6e4b7c2a35b3ef971a2c64d2e7bb9
ljw244 <ljw244@exeter.ac.uk>
2026-01-17 17:19:46 +0000

    updated pick_value to take the middle number

--- game_functions.py
+++ game_functions.py
@@ -2,8 +2,13 @@ import random
 
 # function to be used by game_1: Guess the Number
 def pick_value(poss_values):
-    x = random.choice(poss_values)   
-    return x
+    
+    return poss_values[len(poss_values) // 2]
+
+
+
+
+
 
 # function to be used in game_2: Higher or Lower
 def check_higher_lower(current_val, next_val, user_input):
@@ -20,7 +25,7 @@ def check_higher_lower(current_val, next_val, user_input):
 
 # function to be used in game_3: Hangman
 def process_guess(letter, board, word):
-    
+
     if letter in word:
         for i, char in enumerate(word):
             if char == letter:
